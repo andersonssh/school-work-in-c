2d85b88fc1ca16fc4c2bcbfbc8905fa8a8162fd5
Anderson Araújo <81644687+Andssu@users.noreply.github.com>
2021-08-16 15:47:48 -0300

    adicionado restante do código

--- exercicio1.c
+++ exercicio1.c
@@ -22,3 +22,30 @@ int main(){
  
  
    printf("Digite seu salário: ");
+   scanf("%f",&pessoa.salario);
+ 
+   //calculando alíquota baseada no salário
+   if(pPessoa->salario >=0 &&pPessoa->salario <= 1903.98){
+       aliquota = 0;
+   }else{
+       if(pPessoa->salario <= 2826.65){
+           aliquota = 7.5;
+       }else{
+           if(pPessoa->salario <= 3751.05){
+               aliquota = 15.0;
+           }else{
+               if(pPessoa->salario <= 4664.68){
+                   aliquota = 22.5;
+               }else{
+                   aliquota = 27.5;
+               }
+           }
+       }
+   }
+   //Exibindo todos as informações e calculando valor a ser deduzido do salário
+   printf("Nome do funcionario: %sSalario: %.2f\nAliquota: %.1f\nValor a ser deduzido do salario: %.2f\n\n", pPessoa->funcionario,pPessoa->salario,aliquota, (pPessoa->salario*aliquota/100));
+ 
+ 
+ 
+}
+
